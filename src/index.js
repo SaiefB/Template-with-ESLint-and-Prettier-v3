633cda8b200840dcc656fb7edbf4c6f23123f8ee
Saief <A.S.Bokor@outlook.com>
2026-03-07 02:13:38 +0000

    add some more ESLint validation clarification

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import "./styles.css";
 
-const unusedVariable = "This variable is not used anywhere in the code";
+const unusedVariable =
+  "This variable is not used anywhere in the code - for ESLint Validation";
